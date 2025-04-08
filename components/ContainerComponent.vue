e046e5dc1331dcbba858b72d51d7d11d7b995c83
sobogd <sobogd@gmail.com>
2025-04-08 18:58:44 +0200

    adaptive

--- components/ContainerComponent.vue
+++ components/ContainerComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="text-white w-[500px] p-8 border bg-gray-800 border-gray-800 rounded-lg flex flex-col gap-5 items-center justify-center"
+    class="text-white w-[500px] max-w-[95dvw] max-h-[95dvh] overflow-y-auto p-8 border bg-gray-800 border-gray-800 rounded-lg flex flex-col gap-5 items-start justify-start"
   >
     <slot />
   </section>
